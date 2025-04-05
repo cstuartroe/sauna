df8cd03d949c4df8ca05c2ea0b5efcfce3f06742
Conor Stuart-Roe <conorstuartroe@gmail.com>
2025-04-04 21:42:46 -0400

    Turn parsing into proper state machine

--- dictionary/parse.go
+++ dictionary/parse.go
@@ -32,11 +32,32 @@ var vowels = map[byte]lang.Vowel{
 	'u': lang.U,
 }
 
+type parseState int
+
+const (
+	// Nothing has been parsed yet
+	stateBegin parseState = 0
+
+	// One consonant has been seen after the most recent vowel, and it's unclear whether it's a coda or onset
+	stateAmbiguousConsonant parseState = 1
+
+	// The most recent phoneme is a consonant that you know is the onset,
+	// either because it's the first phoneme in the word, or because there was another consonant before it
+	statePostOnset parseState = 2
+
+	// One vowel has been seen since the most recent consonant/word start
+	stateOneVowel parseState = 3
+
+	// Two vowels have been seen since the most recent consonant/word start
+	stateTwoVowels parseState = 4
+)
+
 func wordFromRomanization(rom string) (lang.ProtoWord, error) {
 	syllables := []lang.ProtoSyllable{}
 	var atr *bool
 
 	i := 0
+	state := stateBegin
 	var mostRecentConsonant lang.Consonant
 	var currentSyllable lang.ProtoSyllable
 
@@ -44,14 +65,6 @@ func wordFromRomanization(rom string) (lang.ProtoWord, error) {
 		letter := rom[i]
 
 		if vowel, ok := vowels[letter]; ok {
-			if (mostRecentConsonant != lang.Consonant{}) {
-				syllables = append(syllables, currentSyllable)
-				currentSyllable = lang.ProtoSyllable{
-					Onset: mostRecentConsonant,
-				}
-				mostRecentConsonant = lang.Consonant{}
-			}
-
 			if atr == nil {
 				b := lang.IsAtr(vowel)
 				atr = &b
@@ -59,25 +72,47 @@ func wordFromRomanization(rom string) (lang.ProtoWord, error) {
 				return lang.ProtoWord{}, fmt.Errorf("mismatching atr: %q", rom)
 			}
 
-			if currentSyllable.Nucleus == 0 {
+			switch state {
+			case stateBegin, statePostOnset:
 				currentSyllable.Nucleus = vowel.Frontness
-			} else if currentSyllable.Offglide == 0 {
+				state = stateOneVowel
+
+			case stateAmbiguousConsonant:
+				syllables = append(syllables, currentSyllable)
+				currentSyllable = lang.ProtoSyllable{
+					Onset:   mostRecentConsonant,
+					Nucleus: vowel.Frontness,
+				}
+				state = stateOneVowel
+
+			case stateOneVowel:
 				currentSyllable.Offglide = vowel.Frontness
-			} else {
+				state = stateTwoVowels
+
+			case stateTwoVowels:
 				return lang.ProtoWord{}, fmt.Errorf("three vowels in a row: %q", rom)
 			}
 		} else if cons, ok := consonants[letter]; ok {
-			if (mostRecentConsonant != lang.Consonant{}) {
-				if currentSyllable.Coda != 0 {
-					return lang.ProtoWord{}, fmt.Errorf("three consonants in a row: %q", rom)
-				}
+			switch state {
+			case stateBegin:
+				currentSyllable.Onset = cons
+				state = statePostOnset
+			case stateOneVowel, stateTwoVowels:
+				state = stateAmbiguousConsonant
+			case statePostOnset:
+				return lang.ProtoWord{}, fmt.Errorf("too many consonants in a row: %q", rom)
+			case stateAmbiguousConsonant:
 				if !lang.IsValidCoda(mostRecentConsonant) {
 					return lang.ProtoWord{}, fmt.Errorf("invalid coda consonant: %q of %q", letter, rom)
 				}
-				currentSyllable.Coda = cons.MOA
+				currentSyllable.Coda = mostRecentConsonant.MOA
 				syllables = append(syllables, currentSyllable)
-				currentSyllable = lang.ProtoSyllable{}
+				currentSyllable = lang.ProtoSyllable{
+					Onset: cons,
+				}
+				state = statePostOnset
 			}
+
 			mostRecentConsonant = cons
 		} else {
 			return lang.ProtoWord{}, fmt.Errorf("unknown letter %q of %q", letter, rom)
@@ -86,8 +121,16 @@ func wordFromRomanization(rom string) (lang.ProtoWord, error) {
 		i++
 	}
 
-	if (mostRecentConsonant != lang.Consonant{}) {
-		return lang.ProtoWord{}, fmt.Errorf("trailing consonant: %q", rom)
+	switch state {
+	case stateBegin:
+		return lang.ProtoWord{}, fmt.Errorf("empty word")
+	case stateAmbiguousConsonant:
+		if !lang.IsValidCoda(mostRecentConsonant) {
+			return lang.ProtoWord{}, fmt.Errorf("invalid final consonant: %q", rom)
+		}
+		currentSyllable.Coda = mostRecentConsonant.MOA
+	case statePostOnset:
+		return lang.ProtoWord{}, fmt.Errorf("word ends with two consonants: %q", rom)
 	}
 
 	syllables = append(syllables, currentSyllable)
