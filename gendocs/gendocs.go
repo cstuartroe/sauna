f24f620daee58bf763618f54df2fd0fa3ed25520
Conor Stuart-Roe <conorstuartroe@gmail.com>
2025-04-04 23:26:02 -0400

    Make North Wind contain full gloss

--- gendocs/gendocs.go
+++ gendocs/gendocs.go
@@ -537,23 +537,50 @@ func GenerateReadme() string {
 func GenerateNorthWind() string {
 	content := ""
 
-	content += section(gl("north-GEN wind-GEN companion-P2/3S-EQU sun-TOP some CL.human-NOM-INT ABL strong-CNJ-GEN-DAT fight-PST-SP"))
+	content += section(glossedSentence(
+		"north-GEN wind-GEN companion-P2/3S-EQU sun-TOP some CL.human-NOM-INT ABL strong-CNJ-GEN-DAT fight-PST-SP",
+		"The North Wind and the Sun were disputing about who was stronger.",
+	))
 
-	content += section(gl("that-ABL-TOP cozy cloth-NOM carry travel man-NOM near-ALL go-PST-S2/3S"))
+	content += section(glossedSentence(
+		"that-ABL-TOP cozy cloth-NOM carry travel man-NOM near-ALL go-PST-S2/3S",
+		"Then a traveling man wearing a warm cloth came by.",
+	))
 
-	content += section(gl("3P-TOP cloth-NOM remove-CAU-CNJ succeed CL.human-NOM ABL strong CL.human-EQU call-PSV-CNJ-LOC one way-NOM be-PST-S2/3S"))
+	content += section(glossedSentence(
+		"3P-TOP cloth-NOM remove-CAU-CNJ succeed CL.human-NOM ABL strong CL.human-EQU call-PSV-CNJ-LOC one way-NOM be-PST-S2/3S",
+		"They agreed that the one who succeeded in making the traveler remove the cloak should be called stronger.",
+	))
 
-	content += section(gl("that-ABL-TOP north-GEN wind-NOM strenuous-EQU blow-PST-S2/3S"))
+	content += section(glossedSentence(
+		"that-ABL-TOP north-GEN wind-NOM strenuous-EQU blow-PST-S2/3S",
+		"Then the North Wind blew as hard as possible.",
+	))
 
-	content += section(gl("but north-GEN wind-TOP some-EQU strenuous-EQU blow time-LOC travel man-NOM that-EQU cuddle-EQU cloth-NOM wrap-PST-S2/3S"))
+	content += section(glossedSentence(
+		"but north-GEN wind-TOP some-EQU strenuous-EQU blow time-LOC travel man-NOM that-EQU cuddle-EQU cloth-NOM wrap-PST-S2/3S",
+		"But the harder the North Wind blew, the more tightly the traveling man wrapped his cloth.",
+	))
 
-	content += section(gl("ending-P2/3S-TOP north-GEN wind-NOM abate-PST-S2/3S"))
+	content += section(glossedSentence(
+		"ending-P2/3S-TOP north-GEN wind-NOM abate-PST-S2/3S",
+		"In the end, the North Wind gave up.",
+	))
 
-	content += section(gl("that-ABL-TOP sun-NOM hot-CNJ shine-PST-S2/3S"))
+	content += section(glossedSentence(
+		"that-ABL-TOP sun-NOM hot-CNJ shine-PST-S2/3S",
+		"Then the Sun shone warmly",
+	))
 
-	content += section(gl("travel man-NOM immediately cloth-NOM remove-PST-S2/3S"))
+	content += section(glossedSentence(
+		"travel man-NOM immediately cloth-NOM remove-PST-S2/3S",
+		"The traveling man immediately removed his cloth.",
+	))
 
-	content += section(gl("this way-TOP north-GEN wind-NOM sun-GEN ABL strong CL.human-EQU call-CAU-PSV-PST-S2/3S"))
+	content += section(glossedSentence(
+		"this way-TOP north-GEN wind-NOM sun-GEN ABL strong CL.human-EQU call-CAU-PSV-PST-S2/3S",
+		"In this way, the North Wind had to call the Sun the stronger one.",
+	))
 
 	return content
 }
